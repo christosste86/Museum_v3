9c379808a2eaf26b3861972537d85430287f0994
Christos <christosste86@gmail.com>
2024-05-25 13:58:58 +0200

    add setDatesToStatisticTable() method

--- functions.java
+++ functions.java
@@ -1,4 +1,5 @@
 import java.sql.Date;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class functions {
@@ -11,12 +12,23 @@ public class functions {
     private DateTimeFormatter czDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     //Statistics
-    private Object [][] table;
+    private Object [][] table = new Object[13][365 - LocalDate.now().getDayOfYear()];
     private final int ticketID = 11;
     private final int paymentID = 12;
+    private final int dateID = 0;
 
     //Tickets
     private Double ticketPrice;
     private Integer maxTicketsPerDay;
+
+    public void setDatesToStatisticTable() {
+        for (int i = 0; i < table[dateID].length; i++) {
+            table[dateID][i] = LocalDate.now().plusDays(i);
+        }
+    }
+
+    public void setRundomUsersToStatisticTable(){
+
+    }
 
 }
