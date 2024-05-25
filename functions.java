ebbd2726294a9e6f55583c78a9a0d2c405e3b18d
Christos <christosste86@gmail.com>
2024-05-25 13:37:26 +0200

    add variables to functions

--- functions.java
+++ functions.java
@@ -1,2 +1,22 @@
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
+
 public class functions {
+    //customers
+    private String firstname;
+    private String lastName;
+    private String status;
+
+    //date Format
+    private DateTimeFormatter czDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    //Statistics
+    private Object [][] table;
+    private final int ticketID = 11;
+    private final int paymentID = 12;
+
+    //Tickets
+    private Double ticketPrice;
+    private Integer maxTicketsPerDay;
+
 }
